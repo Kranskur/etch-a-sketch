6700328d898acaf8640f0d42cc6433e4897e9175
Roque <roquediazv1@gmail.com>
2024-04-23 19:42:30 -0400

    Added algorithm to add divs for container

---
+++ script.js
@@ -0,0 +1,9 @@
+
+for(let i=0; i < 256; i++){
+    const container = document.querySelector("div.container");
+    const div = document.createElement("div");
+    div.classList.add("grid")
+    container.appendChild(div)
+    }
+
+
